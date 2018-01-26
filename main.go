5e6164ca0d55abe615dbe1b82defd53e646380cc
James Dunne <james.jdunne@gmail.com>
2018-01-26 14:56:17 -0600

    Split out parse and emit

--- main.go
+++ main.go
@@ -6,6 +6,64 @@ import (
 	"github.com/cznic/cc"
 )
 
+func parseFunction(fnDecl *cc.Declarator) Function {
+	fn := fnDecl.DirectDeclarator
+
+	f := Function{
+		identifier: identifierOf(fn),
+		ResultType: Type{fnDecl.Type.Result()},
+	}
+
+	pList := fn.ParameterTypeList.ParameterList
+	if pList.ParameterList == nil && pList.ParameterDeclaration.Declarator == nil {
+		// empty void parameter list
+		// TODO: check for 'void' type?
+	} else {
+		for pList != nil {
+			p := pList.ParameterDeclaration
+			if p.Declarator != nil {
+				f.Parameters = append(f.Parameters, Parameter{
+					identifier: identifierOf(p.Declarator.DirectDeclarator),
+					Type:       Type{p.Declarator.Type},
+				})
+			}
+			pList = pList.ParameterList
+		}
+	}
+
+	return f
+}
+
+func emitFunction(f Function) {
+	// Function declaration:
+	fmt.Printf("func %s(\n", f.GoName())
+	for _, p := range f.Parameters {
+		fmt.Printf("\t%s %s,\n", p.GoName(), p.Type.GoType())
+	}
+	if f.ResultType.Kind() == cc.Void {
+		fmt.Printf(")")
+	} else {
+		fmt.Printf(") %s", f.ResultType.GoType())
+	}
+
+	// Function body:
+	fmt.Printf(" {\n")
+	fmt.Printf("\t")
+	if f.ResultType.Kind() != cc.Void {
+		fmt.Printf("return ")
+	}
+	fmt.Printf("C.%s(\n", f.CName())
+	for _, p := range f.Parameters {
+		if p.Type.RequiresCast() {
+			fmt.Printf("\t\t(%s)(%s),\n", p.Type.CGoType(), p.GoName())
+		} else {
+			fmt.Printf("\t\t%s,\n", p.GoName())
+		}
+	}
+	fmt.Printf("\t)\n")
+	fmt.Printf("}\n")
+}
+
 func main() {
 	// Use 64-bit C types model:
 	model := models[Arch64]
@@ -25,78 +83,40 @@ func main() {
 //#include "VG/openvg.h"
 import "C"`)
 
+	functions := make([]Function, 0, 50)
+	//enums := make([]Enum, 0, 50)
+
 	u := tu
 	for u != nil {
-		d := u.ExternalDeclaration
-		if d.Case != 1 {
+		e := u.ExternalDeclaration
+		if e.Case != 1 {
 			continue
 		}
 
 		// Declaration
-		decl := d.Declaration
+		decl := e.Declaration
 		if decl == nil {
 			u = u.TranslationUnit
 			continue
 		}
 
-		fnDecl := decl.InitDeclaratorListOpt.InitDeclaratorList.InitDeclarator.Declarator
-		fn := fnDecl.DirectDeclarator
-		if fn.ParameterTypeList == nil {
-			fmt.Println(identifierOf(fn))
-			u = u.TranslationUnit
-			continue
-		}
-
-		f := Function{
-			identifier: identifierOf(fn),
-			ResultType: Type{fnDecl.Type.Result()},
-		}
-
-		pList := fn.ParameterTypeList.ParameterList
-		if pList.ParameterList == nil && pList.ParameterDeclaration.Declarator == nil {
-			// empty void parameter list
-			// TODO: check for 'void' type?
+		d := decl.InitDeclaratorListOpt.InitDeclaratorList.InitDeclarator.Declarator
+		dd := d.DirectDeclarator
+		if dd.ParameterTypeList != nil {
+			f := parseFunction(d)
+			functions = append(functions, f)
 		} else {
-			for pList != nil {
-				p := pList.ParameterDeclaration
-				if p.Declarator != nil {
-					f.Parameters = append(f.Parameters, Parameter{
-						identifier: identifierOf(p.Declarator.DirectDeclarator),
-						Type:       Type{p.Declarator.Type},
-					})
-				}
-				pList = pList.ParameterList
+			if d.Type.Kind() == cc.Enum {
+				fmt.Println(d)
+				//en := parseEnum(d)
+				//enums = append(enums, en)
 			}
 		}
-
-		// Function declaration:
-		fmt.Printf("func %s(\n", f.GoName())
-		for _, p := range f.Parameters {
-			fmt.Printf("\t%s %s,\n", p.GoName(), p.Type.GoType())
-		}
-		if f.ResultType.Kind() == cc.Void {
-			fmt.Printf(")")
-		} else {
-			fmt.Printf(") %s", f.ResultType.GoType())
-		}
-
-		// Function body:
-		fmt.Printf(" {\n")
-		fmt.Printf("\t")
-		if f.ResultType.Kind() != cc.Void {
-			fmt.Printf("return ")
-		}
-		fmt.Printf("C.%s(\n", f.CName())
-		for _, p := range f.Parameters {
-			if p.Type.RequiresCast() {
-				fmt.Printf("\t\t(%s)(%s),\n", p.Type.CGoType(), p.GoName())
-			} else {
-				fmt.Printf("\t\t%s,\n", p.GoName())
-			}
-		}
-		fmt.Printf("\t)\n")
-		fmt.Printf("}\n")
 
 		u = u.TranslationUnit
+	}
+
+	for _, f := range functions {
+		emitFunction(f)
 	}
 }
